c083da7616f7075c54211ebb8cdd0d98dee68b7a
Lakash dangol <dangol.lakash@gmail.com>
2022-06-27 09:56:39 +0545

    Upto day 25

--- day_20_to_day_un_loginSystem/partials/_header.php
+++ day_20_to_day_un_loginSystem/partials/_header.php
@@ -17,7 +17,12 @@
 
                 if (isset($_SESSION['admin']) && $_SESSION['admin'] == true) {
                     echo '<li class="nav-item">
-                     <a class="nav-link" id="addStudent" href="/lakashojt/day_20_to_day_un_loginSystem/admin/addStudent.php">Add Student</a>
+                     <a class="nav-link" id="addStudent" href="/lakashojt/day_20_to_day_un_loginSystem/admin/addStudent.php">
+                     <svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                    </svg>
+                     Add Student</a>
                     </li>
                 <li class="nav-item">
                      <a class="nav-link" id="showStudent" href="/lakashojt/day_20_to_day_un_loginSystem/admin/showStudent.php">Show Student</a>
@@ -39,7 +44,14 @@
                         <p class="text-light m-0 ms-2">' . $_SESSION['username'] . '</p>
                     </div>
                 
-                    <a href="/lakashojt/day_20_to_day_un_loginSystem/partials/_handleLogout.php" class="btn btn-danger">Logout</a>
+                    <a type="button" href="/lakashojt/day_20_to_day_un_loginSystem/partials/_handleLogout.php" class="btn btn-danger">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"></path>
+                        <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"></path>
+                    </svg>
+                    Logout
+                </a>
+                    
                     
                
                     ';
@@ -55,3 +67,4 @@
     </div>
     <!-- NavBar ends Here  -->
 </nav>
+
